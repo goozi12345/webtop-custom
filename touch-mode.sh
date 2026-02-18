da1453c191d131430cdca24d9f894ee4f0b4408b
goozi12345 <35043673+goozi12345@users.noreply.github.com>
2026-02-18 11:13:44 +0100

    Adjust ComicTagger window in tablet mode
    Added xdotool commands to adjust the size and position of the ComicTagger window in tablet mode.

--- touch-mode.sh
+++ touch-mode.sh
@@ -16,6 +16,9 @@ if [ "$WIDTH" -lt 800 ]; then
     export GDK_SCALE=1
     export QT_TOUCHSCREEN_CALIBRATION=1
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+    APP=$(xdotool search --onlyvisible --name "ComicTagger")
+    xdotool windowsize $APP $WIN_W $WIN_H
+    xdotool windowmove $APP 0 0
 elif [ "$WIDTH" -lt 1400 ]; then
     # Tablet mode
     ICON=$(( $ICON * 1.2 ))
@@ -24,6 +27,9 @@ elif [ "$WIDTH" -lt 1400 ]; then
     export GDK_SCALE=1.2
     export QT_TOUCHSCREEN_CALIBRATION=1
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+    APP=$(xdotool search --onlyvisible --name "ComicTagger")
+    xdotool windowsize $APP $WIN_W $WIN_H
+    xdotool windowmove $APP 0 0
 else
     # Desktop mode
     ICON=$ICON
@@ -36,6 +42,7 @@ fi
 xfconf-query -c xsettings -p /Xft/DPI -s $DPI
 xfconf-query -c xfce4-panel -p /panels/panel-0/size -s $PANEL
 xfconf-query -c xfce4-desktop -p /desktop-icons/icon-size -s $ICON
+
 
 # Optional: Enable touchscreen mode
 xfconf-query -c xsettings -p /Gtk/TouchscreenMode -s 1
